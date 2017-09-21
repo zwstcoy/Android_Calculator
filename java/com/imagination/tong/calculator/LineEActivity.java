52e208ed093978f11c042d828cbc913836b187e9
Tong <zwstcoy@gmail.com>
2017-09-21 00:08:19 -0400

    Add files via upload

--- java/com/imagination/tong/calculator/LineEActivity.java
+++ java/com/imagination/tong/calculator/LineEActivity.java
@@ -17,45 +17,81 @@ public class LineEActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_equation);
-        lineButton=(Button)findViewById(R.id.line_button);
-        lineButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
-
+        lineButton=(Button)findViewById(R.id.find_line_button);
         linePoint=(EditText)findViewById(R.id.line_point);
         lineVector=(EditText)findViewById(R.id.line_vector);
+        answer=(TextView)findViewById(R.id.line_answer);
+
         lineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String point=linePoint.getContext().toString();
-                String vector=lineVector.getContext().toString();
+                String point=linePoint.getText().toString();
+                String vector=lineVector.getText().toString();
+                String str="Your Miss one";
+                if(point!=""&&vector!="") {
+                    str = lineEq(conver(point), conver(vector));
+                }
+                answer.setText(str);
+
             }
         });
-
     }
-    public int[] conven(String str){
 
-        int a=0,b=0,c=0,n=0;
-        int i=str.length();
-        while(str.charAt(i)!=' '&&i<str.length()){
-            a=str.charAt(i)-'0';
-            a=a*(n++)+a;
-        }
-        n=0;
-        while(str.charAt(i)!=' '&&i<str.length()){
-            b=str.charAt(i)-'0';
-            b=b*(n++)+b;
-        }
-        while(str.charAt(i)!=' '&&i<str.length()){
-            c=str.charAt(i)-'0';
-            c=c*(n++)+c;
+    public Point conver(String str){
+        double a=0,b=0,c=0;
+        int i=0,k=10;
+        double n=1;
+        while(str.charAt(i)!=' '&&i<str.length()) {
+            if(str.charAt(i)=='.'){
+                n=.1;
+                k=1;
+            }
+            a = a * k + (n*str.charAt(i++)-'0');
 
         }
-        int list[]={a,b,c};
+        n=1;
+        k=10;
+        i++;
+        while(str.charAt(i)!=' '&&i<str.length()){
+            if(str.charAt(i)=='.'){
+                n=.1;
+                k=1;
+            }
+            b=b*(k)+(str.charAt(i++)-'0')*n;
 
-        return list;
+        }
+        i++;
+        n=1;
+        k=10;
+        while(i<str.length()){
+            if(str.charAt(i)=='.'){
+                n=.1;
+                k=1;
+            }
+            c=c*k+(n*str.charAt(i++)-'0');
+
+
+        }
+        double list[]={a,b,c};
+        Point point =new Point(list);
+        return point;
+    }
+    public String lineEq(Point point,Point vector){
+                 return "X="+point.x+"+"+vector.x+"t\n"+
+                        "Y="+point.y+"+"+vector.y+"t\n"+
+                        "Z="+point.z+"+"+vector.z+"t";
+    }
+
+}
+class Point{
+    double x,y,z;
+    public Point(double list[]){
+        this.x=list[0];
+        this.y=list[1];
+        this.z=list[2];
+    }
+    public String toString(){
+        return "("+x+", "+y+", "+z+")";
     }
 }
--- java/com/imagination/tong/calculator/PlaneEActivity.java
+++ java/com/imagination/tong/calculator/PlaneEActivity.java
@@ -2,13 +2,61 @@ package com.imagination.tong.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Tong on 8/25/2017.
  */
 public class PlaneEActivity extends Activity {
+    Button plantButton;
+    TextView answer;
+    EditText plantPoint,plantVector;
+    LineEActivity lineActivity=new LineEActivity();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plane_equation);
+
+        plantButton=(Button)findViewById(R.id.find_plant_button);
+        plantPoint=(EditText)findViewById(R.id.plane_point);
+        plantVector=(EditText)findViewById(R.id.plane_vector);
+        answer=(TextView)findViewById(R.id.plane_answer);
+
+        plantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String point=plantPoint.getText().toString();
+                String vector=plantVector.getText().toString();
+                String str="Your Miss one";
+                if(point!=""&&vector!="") {
+                    Plane plane=new Plane(lineActivity.conver(point),lineActivity.conver(vector));
+                    str=plane.toString();
+                }
+                answer.setText(str);
+            }
+        });
+
+    }
+}
+class Plane {
+    double sum;
+    Point norVector, point;
+
+    public Plane(Point norVector, Point point) {
+        this.norVector = norVector;
+        this.point = point;
+        sum = (norVector.x * -point.x) + (norVector.y * -point.y) + (norVector.z * -point.z);
+    }
+
+    public String toString() {
+
+        return norVector.x + "(X+" + -point.x + ")+" + norVector.y + "(Y+" + -point.y + ")+" + norVector.z + "(Z+" + -point.z + ")=0\n" +
+                norVector.x + "X+" + norVector.y + "Y+" + norVector.z + "Z=" + sum;
     }
 }
